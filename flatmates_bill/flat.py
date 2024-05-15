020b6bc9332dfce9057d034580271607f9f717d5
Swetha Ganapathi Raman <swethag04@gmail.com>
2024-05-14 20:54:29 -0700

    Create flat.py

---
+++ flatmates_bill/flat.py
@@ -0,0 +1,24 @@
+class Bill:
+    """
+    Object that contains data about a bill such as total amount
+    and period of the bill
+    """
+
+    def __init__(self,amount,period):
+        self.period = period
+        self.amount = amount
+
+class Flatmate:
+    """
+    Creates a flatmate who lives in the flat and pays a share of the bill
+    """
+
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill, flatmate):
+        weight = self.days_in_house/(self.days_in_house + flatmate.days_in_house)
+        bill_share = '$' + str(round((bill.amount)*weight,2))
+        return bill_share
+        
